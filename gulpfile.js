4ca232f56d50c7c94d8e1358072fd4f2cd9c3ab2
Anton Sobolevsky <boapik123@gmail.com>
2016-10-03 19:17:20 +0300

    Add Pug and Sylus files

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,40 @@
 const gulp        = require('gulp');
 const browserSync = require('browser-sync').create();
 const reload      = browserSync.reload;
+const pug         = require('gulp-pug');
+const clean       = require('gulp-clean');
+const stylus      = require('gulp-stylus');
 
 // Static server
 gulp.task('serve', function() {
   browserSync.init({
     server: {
-      baseDir: "./app"
+      baseDir: "./dist"
     }
   });
 
-  gulp.watch("./app/*.html").on("change", reload);
+  gulp.watch("app/pages/*.pug", ['pug']);
+  gulp.watch("app/stylus/**/*.styl", ['stylus']);
+  gulp.watch("dist/*.html").on('change', reload);
 });
+
+gulp.task('pug', function() {
+  return gulp.src('app/pages/*.pug')
+  .pipe(pug({
+    pretty: true
+  }))
+  .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('stylus', function () {
+  return gulp.src('./app/stylus/style.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('clean', function() {
+  return gulp.src('./dist')
+    .pipe(clean());
+});
+
+gulp.task('default', ['pug', 'stylus', 'serve']);
